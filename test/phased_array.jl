1b209225b1d6637444b227fa4a18ed9fed2c2328
Sören Marcus Zorn <soeren.zorn@rwth-aachen.de>
2018-05-08 10:36:41 +0200

    Added crosstalk amplitude test

--- test/phased_array.jl
+++ test/phased_array.jl
@@ -1,21 +1,13 @@
 @testset "Gain and phase mismatch and crosstalk" begin
 
-    num_ants = 4
-    num_sats = 100
-    gen_gen_noise = GNSSSimulator.init_gen_gain_and_phase_mism_and_crosstalk(
-            num_ants,
-            init_phase_mism_betw_ant_std,
-            phase_mism_over_time_std,
-            init_gain_mism_betw_ant_std,
-            gain_mism_over_time_std,
-            init_crosstalk_to_direct_power_dB,
-            init_crosstalk_ampl_std,
-            init_crosstalk_phase_std,
-            crosstalk_ampl_over_time_std,
-            crosstalk_phase_over_time_std
-        )
-        
-    noise = gen_gen_noise(0, trues(num_sats))
-    @test noise * noise' / num_sats ≈ eye(num_ants) atol = 0.4
+    @testset "Crosstalk amplitude"
+        crosstalk_dB = -10
+        gp_mism_crosstalk = GNSSSimulator.init_gen_gain_and_phase_mism_and_crosstalk(2, 0, 0, 0, 0, crosstalk_dB, 0, 0, 0, 0)
+
+        @test abs(gp_mism_crosstalk[1,1]) == abs(gp_mism_crosstalk[2,2])
+        @test abs(gp_mism_crosstalk[1,1]) ≈ abs(gp_mism_crosstalk[1,2]) * 10^(crosstalk_dB / 10)
+        @test abs(gp_mism_crosstalk[1,2]) == abs(gp_mism_crosstalk[2,1])
+
+    end
 
 end
