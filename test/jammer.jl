aebc21b358afbb7f7ed20ec2a1696f53be293492
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-09-13 16:59:45 +0200

    fix jammer amplitude calculation, adjust tests

--- test/jammer.jl
+++ test/jammer.jl
@@ -6,5 +6,5 @@
     jammer = CWJammer(1, CartesianFromSpherical()(Spherical(0.0, 0.0, 1.0)), 0.0m / 1s, jammer_power, true)
     measurement = @inferred GNSSSimulator.init_sim_emitter_signal(jammer, gnss_system, sample_freq, interm_freq)
     next_measurement, signal, internal_states = @inferred measurement(100)
-    @test signal ≈ cis.(2π * interm_freq / sample_freq * (1:100)) * uconvertrp(NoUnits, jammer_power)
+    @test signal ≈ cis.(2π * interm_freq / sample_freq * (1:100)) * sqrt(uconvertp(NoUnits, jammer_power) * sample_freq / 1Hz)
 end
