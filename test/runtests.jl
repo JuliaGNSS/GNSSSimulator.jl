63a4c145e9238f5b36162604912e92668b873f2c
Sören Marcus Zorn <soeren.zorn@rwth-aachen.de>
2019-03-12 13:44:26 +0100

    Remove JuliennedArrays from tests and add Julia 1.0 as a dependency

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using Test, GNSSSimulator, Rotations, CoordinateTransformations, JuliennedArrays, StaticArrays, GNSSSignals, Random, Statistics, Unitful, LinearAlgebra
+using Test, GNSSSimulator, Rotations, CoordinateTransformations, StaticArrays, GNSSSignals, Random, Statistics, Unitful, LinearAlgebra
 import Unitful: dB, Hz, kHz, MHz, GHz, dBHz, m, s, rad, °
 
 const STAT_ATT = RotXYZ(0.1, 0.2, 0.3)
@@ -17,7 +17,7 @@ const DYN_DOA_CART = GNSSSimulator.DynamicDOA(CART_COORD_VEC, 1.0Hz)
 const DYN_DOA_SPH = GNSSSimulator.DynamicDOA(SPH_COORD_VEC, 1.0Hz)
 const LOTHARS_DOAS = [0.6409    0.5260   -0.6634    0.8138   -0.5000   -0.9513   -0.6634         0    0.4924   -0.3100         0;
                      -0.6409   -0.0646    0.3830   -0.2962   -0.5000   -0.1677   -0.5567   -0.0872    0.4132    0.8517   -0.9659;
-                      0.4226    0.8480    0.6428    0.5000    0.7071    0.2588    0.5000    0.9962    0.7660    0.4226    0.2588]    
+                      0.4226    0.8480    0.6428    0.5000    0.7071    0.2588    0.5000    0.9962    0.7660    0.4226    0.2588]
 const NUM_ANTS = 4
 const EARTH_RADIUS = 6_360_000m
 
